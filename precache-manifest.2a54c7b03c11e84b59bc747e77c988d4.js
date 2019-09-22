e44e9c94ea09f4362c8737a40a08111cad007c72
AKM Islam <mamunjahir.com@gmail.com>
2019-09-21 22:39:36 -0400

    Updates

---
+++ precache-manifest.2a54c7b03c11e84b59bc747e77c988d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a3848a217ca95088a09a36d9c087d2",
+    "url": "/reactstrap_test1/index.html"
+  },
+  {
+    "revision": "31e3dc06cdf0a070afd0",
+    "url": "/reactstrap_test1/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3bfc088d8167d1afe99e",
+    "url": "/reactstrap_test1/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "31e3dc06cdf0a070afd0",
+    "url": "/reactstrap_test1/static/js/2.3222b801.chunk.js"
+  },
+  {
+    "revision": "3bfc088d8167d1afe99e",
+    "url": "/reactstrap_test1/static/js/main.be949311.chunk.js"
+  },
+  {
+    "revision": "010ee82bcfc55ef1ef6e",
+    "url": "/reactstrap_test1/static/js/runtime-main.21cf4c6a.js"
+  }
+]);
